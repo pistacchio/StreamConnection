53c9d2ca4201d0a92eb0f97932f3fd9435cd61b0
gu <pistacchio@gmail.com>
2011-10-27 00:39:37 +0200

    finished cbot

--- bots/c/cbot.cc
+++ bots/c/cbot.cc
@@ -35,7 +35,10 @@ main()
       int sharp = rand() % 24;
       code[sharp] = '#';
 
-      char row[5];
+      char row[6];
+      row[5] = '\0'; // sets the last chat at 0 so that copying 5 chars with
+                     //   strncpy results always as a null termined string
+      
       for (int i=0; i<5; i++)
       {
         strncpy(row, code+(i*5), 5);
@@ -43,7 +46,25 @@ main()
         fflush(stdout);
       }
     }
-    if (strcmp(line, "bye!") == 0) {printf("bye!\n"); fflush(stdout); return 0;}    
+    if (strcmp(line, "find code") == 0)
+    {
+      char code[25];
+
+      // makes a single string out of the 5 rows of code
+      for (int i=0; i<5; i++)      
+      {
+        getline(line);
+        strncpy(code+(i*5), line, 5);
+      }
+      int sharp = strcspn(code, "#");
+      int y = sharp/5;
+      int x = sharp-(y*5);
+      printf("%d %d\n", x, y);
+      fflush(stdout);            
+    }
+    if (strcmp(line, "bye!") == 0) {printf("bye!\n"); fflush(stdout); return 0;}
+    
+    memset(&line, '\0', 20); // resets line to all 0s
   }
   
   return 0;
--- runhost.sh
+++ runhost.sh
@@ -3,4 +3,4 @@
   ant compile jar
 #fi
 
-java -jar build/jar/Host.jar "/Users/gu/dev/org.github.pistacchio.streamcommunication/bots/c/a.out" "/opt/local/bin/python /Users/gu/dev/org.github.pistacchio.streamcommunication/bots/python/pybot.py"
+java -jar build/jar/Host.jar "/Users/gu/dev/org.github.pistacchio.streamcommunication/bots/c/a.out" "/Users/gu/dev/org.github.pistacchio.streamcommunication/bots/c/a.out"
