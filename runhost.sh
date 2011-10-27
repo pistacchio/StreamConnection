123b283642a2fdcb49fd7a0c88e13d7ec6245849
gu <pistacchio@gmail.com>
2011-10-27 08:36:05 +0200

    + cppbot

--- runhost.sh
+++ runhost.sh
@@ -3,4 +3,4 @@
   ant compile jar
 #fi
 
-java -jar build/jar/Host.jar "/Users/gu/dev/org.github.pistacchio.streamcommunication/bots/c/a.out" "/Users/gu/dev/org.github.pistacchio.streamcommunication/bots/c/a.out"
+java -jar build/jar/Host.jar "/Users/gu/dev/org.github.pistacchio.streamcommunication/a.out" "/Users/gu/dev/org.github.pistacchio.streamcommunication/a.out"
