46ab662e004a0d666e5489d87b59010c2291d024
gu <pistacchio@gmail.com>
2011-10-31 17:19:41 +0100

    + README.md

--- runhost.bat
+++ runhost.bat
@@ -2,4 +2,4 @@ rem if not exist "build\jar\Host.jar" (
   call ant compile jar 
 rem )
 
-java -jar build\jar\Host.jar "java -jar cbot.jar" "java -jar cbot.jar"
+java -jar build\jar\Host.jar "python pybot.py" "python pybot.py"
